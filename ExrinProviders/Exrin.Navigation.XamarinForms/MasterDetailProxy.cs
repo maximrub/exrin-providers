91f2165660db3a0167ba2aa19548e17681d1173f
Maxim Rubchinsky <maxim@rubchinsky.com>
2017-07-24 17:46:57 +0300

    added master page proxy

---
+++ ExrinProviders/Exrin.Navigation.XamarinForms/MasterDetailProxy.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Exrin.Abstraction;
+using Xamarin.Forms;
+
+namespace Exrin.Navigation.XamarinForms
+{
+    public class MasterDetailProxy : IMasterDetailProxy
+    {
+        private readonly MasterDetailPage r_MasterPage;
+
+        public MasterDetailProxy(MasterDetailPage i_MasterPage)
+        {
+            View = i_MasterPage;
+            r_MasterPage = i_MasterPage;
+        }
+
+        public object DetailNativeView
+        {
+            get
+            {
+                return r_MasterPage.Detail;
+            }
+
+            set
+            {
+                r_MasterPage.Detail = value as Page;
+            }
+        }
+
+        public object MasterNativeView
+        {
+            get
+            {
+                return r_MasterPage.Master;
+            }
+
+            set
+            {
+                Page page = value as Page;
+                if(string.IsNullOrEmpty(page.Title))
+                {
+                    page.Title = "Please set your MasterPage Title";
+                }
+
+                r_MasterPage.Master = page;
+            }
+        }
+
+        public object View { get; set; }
+    }
+}
