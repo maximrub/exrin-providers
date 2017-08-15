d3e417d83763b293906f60224797a38efa7107a8
Maxim Rubchinsky <maxim@rubchinsky.com>
2017-08-15 11:35:59 +0300

    page proxy

---
+++ ExrinProviders/Exrin.Base/PageProxy.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Exrin.Abstraction;
+using Xamarin.Forms;
+
+namespace Exrin.Base
+{
+    /// <summary>
+    /// Implements IView on all ContentPages. ALL your views must inherit from this proxy.
+    /// </summary>
+    public class PageProxy : ContentPage, IView
+    {
+        protected override bool OnBackButtonPressed()
+        {
+            return ((IView)this).OnBackButtonPressed();
+        }
+
+        Func<bool> IView.OnBackButtonPressed { get; set; }
+    }
+}
